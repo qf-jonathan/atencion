d38ed2e4496cd41500759620334c72ae42eb19e8
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2020-02-20 12:42:18 -0500

    added shared module with button and input reference, login view implemented #6

--- frontend/src/app/pages/login/login.module.ts
+++ frontend/src/app/pages/login/login.module.ts
@@ -2,11 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LoginComponent} from './login.component';
 import {RouterModule} from "@angular/router";
+import {LoginInputComponent} from './login-input/login-input.component';
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, LoginInputComponent],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '',
