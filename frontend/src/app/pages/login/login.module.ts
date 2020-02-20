a14359f741bfc678f47c68d7f936bff64039e86e
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2020-02-19 19:30:30 -0500

    webui and angular project created #6

---
+++ frontend/src/app/pages/login/login.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from './login.component';
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        pathMatch: 'full',
+        component: LoginComponent
+      }
+    ])
+  ]
+})
+export class LoginModule {
+}
