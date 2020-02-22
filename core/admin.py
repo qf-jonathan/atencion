7fbfafd247bfe778f9e57faea9dc41de354d1572
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2020-02-22 08:04:14 -0500

    user admin fixes

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'profile')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'profile'),
+        }),
+    )
+    list_display = ('username', 'profile', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(Group)
